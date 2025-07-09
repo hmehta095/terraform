90817282059dea8e45071f14e38cc7c6f4570545
Himanshu Mehta <31034085+hmehta095@users.noreply.github.com>
2025-07-09 12:00:36 -0400

    Update main.tf
    modules

--- main.tf
+++ main.tf
@@ -21,46 +21,24 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [aws_security_group.blog.id]
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
     Name = "terraform"
   }
 }
 
-resource "aws_security_group" "blog" {
-  name        = "blog"   #it will reflect in aws
-  description = "Allow http and https in. All everything out"
-  
-  vpc_id = data.aws_vpc.default.id
+module "blog_sg"{
+source  = "terraform-aws-modules/security-group/aws"
+version = "5.3.0"
+
+vpc_id = data.aws_vpc.default.id
+name = "blog_new"
+
+vpc_id = data.aws_vpc.default.id
+ingress_rule = ["http-80-tcp", "https-443-tcp"]
+ingress_cidr_blocks = ["0.0.0.0/0"]
+
+egress_rule = ["all-all"]
+egress_cidr_blocks = ["0.0.0.0/0"]
 }
-
-resource "aws_security_group_rule" "blog_http_in" {
-  type        = "ingress"
-  from_port   = 80
-  to_port     = 80
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]  # we are typing this cidr 0.0.0.0/0 because this is public website. You can mention your own IP for security
-  
-  security_group_id = aws_security_group.blog.id
-  }
-
-resource "aws_security_group_rule" "blog_https_in" {
-  type        = "ingress"
-  from_port   = 443
-  to_port     = 443
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]  # we are typing this cidr 0.0.0.0/0 because this is public website. You can mention your own IP for security
-  
-  security_group_id = aws_security_group.blog.id
-  }
-
-resource "aws_security_group_rule" "blog_everything_out" {
-  type        = "egress"
-  from_port   = 0
-  to_port     = 0
-  protocol    = "-1"
-  cidr_blocks = ["0.0.0.0/0"]  
-  
-  security_group_id = aws_security_group.blog.id
-  }
