1953bda2df211b05df40108e83daa03b00142f66
Himanshu Mehta <31034085+hmehta095@users.noreply.github.com>
2025-07-09 12:37:50 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,31 +13,29 @@ data "aws_ami" "app_ami" {
 
   owners = ["979382823631"] # Bitnami
 }
+
 data "aws_vpc" "default" {
-  default = true      # Defining a VPC and terraform has feature to pull the default VPC in aws
+  default = true
 }
 
 resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
-
+  ami                    = data.aws_ami.app_ami.id
+  instance_type          = var.instance_type
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
-    Name = "terraform"
+    Name = "Learning Terraform"
   }
 }
 
-module "blog_sg"{
-source  = "terraform-aws-modules/security-group/aws"
-version = "5.3.0"
-name = "blog"
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "4.13.0"
 
-vpc_id = data.aws_vpc.default.id
-
-ingress_rules = ["http-80-tcp", "https-443-tcp"]
-ingress_cidr_blocks = ["0.0.0.0/0"]
-
-egress_rules = ["all-all"]
-egress_cidr_blocks = ["0.0.0.0/0"]
+  vpc_id  = data.aws_vpc.default.id
+  name    = "blog"
+  ingress_rules = ["https-443-tcp","http-80-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
 }
