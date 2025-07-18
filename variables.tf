c2a5291a9ad0d490cdafaa8ab43f34644ffef0f5
Himanshu Mehta <31034085+hmehta095@users.noreply.github.com>
2025-07-18 13:19:33 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,3 +2,52 @@ variable "instance_type" {
   description = "Type of EC2 instance to provision"
   default     = "t3.nano"
 }
+
+variable "ami_filter"{
+  description = "Name filter and owner for the AMI"
+
+  type = object({
+    name  = string
+    owner = string
+  })
+  default = {
+  name    = "bitnami-tomcat-*-x86_64-hvm-ebs-nami"
+  owners  = "979382823631" # Bitnami
+  }
+}
+
+variable "environment" {
+  description = "Development Environement"
+  
+  type = object({
+    name           = string
+    network_prefix = string
+  })
+
+  default = {
+    name           = "dev"
+    network_prefix = "10.0"
+  }
+}
+
+name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a","us-west-2b","us-west-2c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
+
+variable "min_size" {
+  description = "Minimum number of instance in ASG"
+  default     = 1
+}
+variable "max_size" {
+  description = "Maximum number of instance in ASG"
+  default     = 2
+}
