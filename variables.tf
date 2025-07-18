d252995368ec2afa42b5b16f48c9fde3cd7996ee
Himanshu Mehta <31034085+hmehta095@users.noreply.github.com>
2025-07-18 14:16:12 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -30,19 +30,6 @@ variable "environment" {
   }
 }
 
-name = "dev"
-  cidr = "10.0.0.0/16"
-
-  azs             = ["us-west-2a","us-west-2b","us-west-2c"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  tags = {
-    Terraform = "true"
-    Environment = "dev"
-  }
-}
-
-
 variable "asg_min_size" {
   description = "Minimum number of instance in ASG"
   default     = 1
