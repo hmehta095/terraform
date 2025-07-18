0adb529e5d1aa7b40ba61dd3cad8f0fa3b67d8ae
Himanshu Mehta <31034085+hmehta095@users.noreply.github.com>
2025-07-17 22:04:20 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ module "blog_autoscaling" {
   min_size            = 1
   max_size            = 2
   vpc_zone_identifier = module.blog_vpc.public_subnets
-  autoscaling_group_tags   = module.blog_alb.target_group
+  autoscaling_group_tags   = module.blog_alb.target_groups
   security_groups     = [module.blog_sg.security_group_id]
   instance_type       = var.instance_type
   image_id            = data.aws_ami.app_ami.id
